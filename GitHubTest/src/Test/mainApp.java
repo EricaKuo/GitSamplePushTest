24902a5eac0b909e06b3090ba078d6e0778cda71
Yvonne <n_kuo1994@hotmail.com.tw>
2021-06-08 15:15:35 +0800

    3rd version test

--- GitHubTest/src/Test/mainApp.java
+++ GitHubTest/src/Test/mainApp.java
@@ -4,7 +4,8 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("2nd version");
+		System.out.println("3rd version");
+		
 
 	}
 
